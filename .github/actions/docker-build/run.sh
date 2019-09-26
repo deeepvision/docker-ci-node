3c30abc918df7bb5a11e5cdb2971b4386ef249a7
Sergii Sadovyi <serg.sadovoi@gmail.com>
2019-09-26 15:53:54 +0300

    build: publish to docker hub

--- .github/actions/docker-build/run.sh
+++ .github/actions/docker-build/run.sh
@@ -6,5 +6,8 @@ set -u
 cd /github/workspace
 
 docker login docker.pkg.github.com -u sergsadovyi -p $GITHUB_DOCKER_TOKEN
-docker build -t docker.pkg.github.com/deeepvision/docker-ci-node/ci-node .
-docker push docker.pkg.github.com/deeepvision/docker-ci-node/ci-node
+docker pull docker.pkg.github.com/deeepvision/docker-ci-node/ci-node
+
+docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASS
+docker tag docker.pkg.github.com/deeepvision/docker-ci-node/ci-node deeepvision/ci-node
+docker push deeepvision/ci-node
