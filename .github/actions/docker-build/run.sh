48ac8c9f6714d209f17d20d0231e0a2d6f7e627a
Sergii Sadovyi <serg.sadovoi@gmail.com>
2019-09-26 11:33:33 +0300

    build: login and publish to local docker

--- .github/actions/docker-build/run.sh
+++ .github/actions/docker-build/run.sh
@@ -6,6 +6,6 @@ set -u
 echo "Executing Docker action"
 cd /github/workspace
 
-#docker login docker.pkg.github.com -u $GITHUB_DOCKER_USER -p $GITHUB_DOCKER_TOKEN
+docker login docker.pkg.github.com -u sergsadovoi -p $GITHUB_TOKEN
 docker build -t docker.pkg.github.com/deeepvision/docker-ci-node/ci-node .
 docker push docker.pkg.github.com/deeepvision/docker-ci-node/ci-node
